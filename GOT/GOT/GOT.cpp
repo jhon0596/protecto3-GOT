381e3ed88e37f362a08de5287526171b3c76c0af
g74253 <38565604+g74253@users.noreply.github.com>
2020-08-11 22:07:16 -0600

    Consola inicial y MD5
    Se agrego la consola inicial con los comandos base y el algoritmo MD5.

--- GOT/GOT/GOT.cpp
+++ GOT/GOT/GOT.cpp
@@ -1,180 +1,98 @@
-// GOT.cpp : Define el punto de entrada de la aplicación.
-//
+#include <string>
+#include <iostream>
+#include "md5.h"
 
-#include "framework.h"
-#include "GOT.h"
 
-#define MAX_LOADSTRING 100
+using namespace std;
 
-// Variables globales:
-HINSTANCE hInst;                                // instancia actual
-WCHAR szTitle[MAX_LOADSTRING];                  // Texto de la barra de título
-WCHAR szWindowClass[MAX_LOADSTRING];            // nombre de clase de la ventana principal
-
-// Declaraciones de funciones adelantadas incluidas en este módulo de código:
-ATOM                MyRegisterClass(HINSTANCE hInstance);
-BOOL                InitInstance(HINSTANCE, int);
-LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
-INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
-
-int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-                     _In_opt_ HINSTANCE hPrevInstance,
-                     _In_ LPWSTR    lpCmdLine,
-                     _In_ int       nCmdShow)
+int main()
 {
-    UNREFERENCED_PARAMETER(hPrevInstance);
-    UNREFERENCED_PARAMETER(lpCmdLine);
+	bool inicio = true;
+	string comando;
+	cout <<md5("hola")<< "\n\n";
+    cout << "Bienvenido a GOT\n\n";
+    while (inicio) {
+		cout <<"Dijite su comando :" <<endl;
+		getline(cin ,comando);
+		if (comando.substr(0,4) == "got ") {
+			if (comando.substr(4,4)== "exit"){
+				inicio = false;
+			}
+			else if (comando.substr(4, 5) == "init ") {
+				//funcion de init 
+				cout << comando.substr(9) << endl; 
+			}
+			else if (comando.substr(4, 4) == "help") {
+				cout << "Bienvenidos a la ayuda de GOT\n\n"
+					<< "got init <name>\n"
+					<< "Instancia un nuevo repositorio en el servidor y lo identifica con el nombre indicado por <name>.\n\n"
+					<< "got add [-A] [name]\n"
+					<< "Permite agregar todos los archivos que no esten registrados o que tengan nuevos cambios al repositorio. Puede usar -A para registrar todos los archivos o poner nombre para registrar solo uno.\n\n"
+					<< "got commit <mensaje>\n"
+					<< "Envia los archivos agregados y pendientes de commit al servidor. Se debe especificar un mensaje.\n\n"
+					<< "got  status <file>\n"
+					<< "Este comando en caso de no especificar archivo nos va a mostrar cuales archivos han sido cambiados, agregados o eliminados de acuerdo con el commit anterior.Se puede poner nombre para ver todo el historial del archivo.\n\n"
+					<< "got rollback <file> <commit>\n"
+					<< "Permite regresar un archivo en el tiempo a un commit especifico. \n\n"
+					<< "got reset <file>\n"
+					<< "Deshace cambios locales para un archivo y lo regresa al ultimo commit.\n\n"
+					<< "got sync <file>\n"
+					<< "Recupera los cambios para un archivo en el servidor y lo sincroniza con el archivo en el cliente. \n\n"
+					<< "got exit\n"
+					<< "Termina got\n\n"
+						
+					
+					<< endl;
 
-    // TODO: Colocar código aquí.
+			}
+			else if (comando.substr(4, 4) == "add ") {
+				string aux = comando.substr(8);
+				if (aux == "-A") {
+					cout << "algo" <<endl;
+				}
+				else {
+					cout << aux << endl;
+				}
+			}
 
-    // Inicializar cadenas globales
-    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-    LoadStringW(hInstance, IDC_GOT, szWindowClass, MAX_LOADSTRING);
-    MyRegisterClass(hInstance);
+			else if (comando.substr(4, 7) == "commit ") {
+				//funcion de commit
+				cout << comando.substr(11) << endl;
 
-    // Realizar la inicialización de la aplicación:
-    if (!InitInstance (hInstance, nCmdShow))
-    {
-        return FALSE;
-    }
+			}
+			else if (comando.substr(4, 7) == "status ") {
+				//funcion de status all
+				cout << comando.substr(11) << endl;
+			}
+			else if (comando.substr(4, 6) == "status") {
+				//funcion de status
+	
+			}
+			else if (comando.substr(4, 9) == "rollback ") {
+				string aux = comando.substr(13);
+				for (int i = 0; i < aux.length(); i++) {
+					if (aux.substr(i, 1) == " ") {
+						cout << "File " << aux.substr(0,i) << endl;
+						cout << "Commit " << aux.substr(i+1) << endl;
+						break;
+					}
+				}
 
-    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_GOT));
-
-    MSG msg;
-
-    // Bucle principal de mensajes:
-    while (GetMessage(&msg, nullptr, 0, 0))
-    {
-        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
-        {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-        }
-    }
-
-    return (int) msg.wParam;
-}
-
-
-
-//
-//  FUNCIÓN: MyRegisterClass()
-//
-//  PROPÓSITO: Registra la clase de ventana.
-//
-ATOM MyRegisterClass(HINSTANCE hInstance)
-{
-    WNDCLASSEXW wcex;
-
-    wcex.cbSize = sizeof(WNDCLASSEX);
-
-    wcex.style          = CS_HREDRAW | CS_VREDRAW;
-    wcex.lpfnWndProc    = WndProc;
-    wcex.cbClsExtra     = 0;
-    wcex.cbWndExtra     = 0;
-    wcex.hInstance      = hInstance;
-    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_GOT));
-    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
-    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
-    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_GOT);
-    wcex.lpszClassName  = szWindowClass;
-    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
-
-    return RegisterClassExW(&wcex);
-}
-
-//
-//   FUNCIÓN: InitInstance(HINSTANCE, int)
-//
-//   PROPÓSITO: Guarda el identificador de instancia y crea la ventana principal
-//
-//   COMENTARIOS:
-//
-//        En esta función, se guarda el identificador de instancia en una variable común y
-//        se crea y muestra la ventana principal del programa.
-//
-BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
-{
-   hInst = hInstance; // Almacenar identificador de instancia en una variable global
-
-   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
-
-   if (!hWnd)
-   {
-      return FALSE;
-   }
-
-   ShowWindow(hWnd, nCmdShow);
-   UpdateWindow(hWnd);
-
-   return TRUE;
-}
-
-//
-//  FUNCIÓN: WndProc(HWND, UINT, WPARAM, LPARAM)
-//
-//  PROPÓSITO: Procesa mensajes de la ventana principal.
-//
-//  WM_COMMAND  - procesar el menú de aplicaciones
-//  WM_PAINT    - Pintar la ventana principal
-//  WM_DESTROY  - publicar un mensaje de salida y volver
-//
-//
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
-{
-    switch (message)
-    {
-    case WM_COMMAND:
-        {
-            int wmId = LOWORD(wParam);
-            // Analizar las selecciones de menú:
-            switch (wmId)
-            {
-            case IDM_ABOUT:
-                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-                break;
-            case IDM_EXIT:
-                DestroyWindow(hWnd);
-                break;
-            default:
-                return DefWindowProc(hWnd, message, wParam, lParam);
-            }
-        }
-        break;
-    case WM_PAINT:
-        {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hWnd, &ps);
-            // TODO: Agregar cualquier código de dibujo que use hDC aquí...
-            EndPaint(hWnd, &ps);
-        }
-        break;
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        break;
-    default:
-        return DefWindowProc(hWnd, message, wParam, lParam);
-    }
-    return 0;
-}
-
-// Controlador de mensajes del cuadro Acerca de.
-INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
-{
-    UNREFERENCED_PARAMETER(lParam);
-    switch (message)
-    {
-    case WM_INITDIALOG:
-        return (INT_PTR)TRUE;
-
-    case WM_COMMAND:
-        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
-        {
-            EndDialog(hDlg, LOWORD(wParam));
-            return (INT_PTR)TRUE;
-        }
-        break;
-    }
-    return (INT_PTR)FALSE;
+			}
+			else if (comando.substr(4, 6) == "reset ") {
+				//funcion de reset
+				cout << comando.substr(10) << endl;
+			}
+			else if (comando.substr(4, 5) == "sync ") {
+				//funcion de sync
+				cout << comando.substr(9) << endl;
+			}
+			else {
+				cout << "GOT no cuenta con este comando" << endl;
+			}
+		}
+		else {
+			cout << "No se acepta ese comando" << endl;
+		}
+	}
 }
